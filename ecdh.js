515a466ce4a0b44fd5182262774ba8a1e316537b
Alexandr Kozlenkov <sah4ez32@gmail.com>
2021-10-17 15:50:55 +0300

    Udated version of nginx and using getRandomValues

--- ecdh.js
+++ ecdh.js
@@ -1,25 +1,20 @@
 import axl from 'axlsign.js'
-// import crypto from 'crypto'
 
 function ToBase64 (u8) {
   return Buffer.from(String.fromCharCode.apply(null, u8)).toString('base64url')
 }
 
 function testEcdh (r) {
-  const seed1 = Buffer.from(r.headersIn['X-Request-Id'])
-  const seed2 = new Uint8Array(32)
-  for (let i = 0; (i < seed1.length && i < seed2.length) ; i++) {
-    seed2[i] = seed1[i]
-  }
-  const keyPair1 = axl.generateKeyPair(seed2)
-  const keyPair2 = axl.generateKeyPair(seed2)
+  const buf = crypto.getRandomValues(new Uint8Array(32))
+  const keyPair1 = axl.generateKeyPair(buf)
+  const keyPair2 = axl.generateKeyPair(buf)
   const pairs1 = ToBase64(keyPair1.public) + ':::' + ToBase64(keyPair2.public)
   const pairs2 = ToBase64(keyPair1.private) + ':::' + ToBase64(keyPair2.private)
 
   const shk1 = axl.sharedKey(keyPair1.private, keyPair2.public)
   const shk2 = axl.sharedKey(keyPair2.private, keyPair1.public)
 
-  r.return(200, ToBase64(shk1) + '::::' + ToBase64(shk2) + ':::' + pairs1 + ':::' + pairs2)
+  r.return(200, ToBase64(shk1) + '::::' + ToBase64(shk2) + ':::' + pairs1 + ':::' + pairs2 + ':::' + ToBase64(buf))
 }
 
 export default { testEcdh }
